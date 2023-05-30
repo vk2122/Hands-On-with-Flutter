ab1f95a870e1b7142f8fce4cac8c616a0588da25
Varun Khachane <varunkhachane31@gmail.com>
2023-05-30 23:27:22 +0530

    Project Initialized :tada:

---
+++ movie_review_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Movie Review App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+    );
+  }
+}
