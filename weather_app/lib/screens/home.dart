8c8bae5b79453600d2cd84711dfd8800c16241b4
Varun Khachane <varunkhachane31@gmail.com>
2023-06-19 13:49:12 +0530

    Project Completed

--- weather_app/lib/screens/home.dart
+++ weather_app/lib/screens/home.dart
@@ -1,15 +1,13 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   String name = "Vapi";
   String desc = "description";
   String windspeed = "0.0";
@@ -19,11 +17,10 @@ class _HomePageState extends State<HomePage> {
 
   var searchCtrl = TextEditingController();
 
-
   @override
   Widget build(BuildContext context) {
-
-    final fetchedMap = ModalRoute.of(context)!.settings.arguments as Map;
+    final fetchedMap =
+        ModalRoute.of(context)!.settings.arguments as Map<String, dynamic>;
     print(fetchedMap);
 
     name = fetchedMap['keyName'];
@@ -33,15 +30,16 @@ class _HomePageState extends State<HomePage> {
     temperature = fetchedMap['keyTemp'];
     windspeed = fetchedMap['keyWindspeed'];
 
-    if (windspeed == "NA" && temperature =="NA"){
-
+    if (windspeed == "NA" && temperature == "NA") {
+    } else {
+      windspeed = windspeed.toString().substring(0,
+          windspeed.toString().length >= 4 ? 4 : windspeed.toString().length);
+      temperature = temperature.toString().substring(
+          0,
+          temperature.toString().length >= 4
+              ? 4
+              : temperature.toString().length);
     }
-    else{
-      windspeed = fetchedMap['keyWindspeed'].toString().substring(0,4);
-      temperature = fetchedMap['keyTemp'].toString().substring(0,4);
-    }
-
-
 
     return Scaffold(
       resizeToAvoidBottomInset: false,
@@ -53,142 +51,148 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: SafeArea(
           child: Container(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
-              child: Column(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(24),
-                      color: Color(0xffc3c8cb),
-                    ),
-                    child: Row(
-                      children: [
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                          child: InkWell(
-                              onTap: (){
-
-                                if (searchCtrl.text.replaceAll(" ", "") != ""){
-
-                                  Navigator.pushReplacementNamed(context, '/loading', arguments: {
-                                    "searchData" : searchCtrl.text.toString()
-                                  });
-                                }
-
-                              },
-                              child: Icon(CupertinoIcons.search)),
-                        ),
-                        Expanded(
-                          child: TextField(
-                            controller: searchCtrl,
-                            decoration: InputDecoration(
-                              hintText: "Search any city name....",
-                              border: InputBorder.none,
-                            ),
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.fromLTRB(20, 0, 10, 0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(24),
+                    color: Color(0xffc3c8cb),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          controller: searchCtrl,
+                          decoration: InputDecoration(
+                            hintText: "Search any city name....",
+                            border: InputBorder.none,
                           ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                        child: InkWell(
+                            onTap: () {
+                              if (searchCtrl.text.replaceAll(" ", "") != "") {
+                                Navigator.pushReplacementNamed(
+                                    context, '/loading', arguments: {
+                                  "searchData": searchCtrl.text.toString()
+                                });
+                              }
+                            },
+                            child: Icon(Icons.search)),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 40),
+                Text(
+                  "$name",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Jost',
+                      color: Color(0xffc3c8cb),
+                      fontSize: 24),
+                ),
+                Text(
+                  "TODAY",
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontFamily: 'Jost',
+                      color: Color(0xffc3c8cb),
+                      fontSize: 12),
+                ),
+                SizedBox(height: 40),
+                Image.network(
+                  "https://openweathermap.org/img/wn/$icon@2x.png",
+                  height: 100,
+                  width: 100,
+                ),
+                Text(
+                  "$desc",
+                  style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontFamily: 'Jost',
+                      color: Color(0xffc3c8cb),
+                      fontSize: 18),
+                ),
+                SizedBox(height: 40),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "Temperature",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontFamily: 'Jost',
+                              color: Color(0xffc3c8cb),
+                              fontSize: 16),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          "$temperature°C",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Jost',
+                              color: Color(0xffc3c8cb),
+                              fontSize: 18),
                         ),
                       ],
                     ),
-                  ),
-                  SizedBox(height: 40,),
-                  Text("$name", style: TextStyle(fontWeight: FontWeight.bold, fontFamily: 'Jost', color: Color(0xffc3c8cb), fontSize: 24)),
-                  Text("TODAY", style: TextStyle(fontWeight: FontWeight.normal, fontFamily: 'Jost', color: Color(0xffc3c8cb), fontSize: 12)),
-                  SizedBox(height: 40,),
-                  Image.network("https://openweathermap.org/img/wn/$icon@2x.png",
-                    height: 100,
-                    width: 100,),
-                  Text("$desc",style: TextStyle(fontWeight: FontWeight.normal, fontFamily: 'Jost', color: Color(0xffc3c8cb), fontSize: 15)),
-
-                  SizedBox(height: 40,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    children: [
-                      Container(
-                        height: 90,
-                        child: Text("$temperature", style: TextStyle(fontFamily: 'Jost', fontWeight: FontWeight.bold, color: Color(0xffc3c8cb), fontSize: 80),),
-                      ),
-                      SizedBox(width: 5,),
-                      Container(
-                        padding: EdgeInsets.only(top: 10,),
-                        height: 90,
-                        child: Column(
-                          children: [
-                            Text("o", style: TextStyle(fontFamily: 'Jost', fontWeight: FontWeight.bold, color: Color(0xffc3c8cb), fontSize: 25),),
-                            Text("   C", style: TextStyle(fontFamily: 'Jost', fontWeight: FontWeight.bold, color: Color(0xffc3c8cb), fontSize: 30),),
-                          ],
+                    Column(
+                      children: [
+                        Text(
+                          "Wind Speed",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontFamily: 'Jost',
+                              color: Color(0xffc3c8cb),
+                              fontSize: 16),
                         ),
-                      ),
-                    ],
-                  ),
-
-                  SizedBox(height: 80,),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Expanded(
-                        child: Container(
-                          height: 180,
-                          padding: EdgeInsets.all(20),
-                          margin: EdgeInsets.only(right: 8),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(14),
-                            color: Colors.white12,
-                          ),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Icon(Icons.water_drop_rounded, color: Color(0xffc3c8cb), size: 35,),
-                                ],
-                              ),
-                              SizedBox(height: 10,),
-                              Text("$humidity", style: TextStyle(fontFamily: 'Jost', fontWeight: FontWeight.bold, color: Color(0xffc3c8cb), fontSize: 40),),
-                              Text("%", style: TextStyle(fontFamily: 'Jost', fontWeight: FontWeight.normal, color: Color(0xffc3c8cb), fontSize: 16),),
-                            ],
-                          ),
+                        SizedBox(height: 10),
+                        Text(
+                          "$windspeed km/h",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Jost',
+                              color: Color(0xffc3c8cb),
+                              fontSize: 18),
                         ),
-                      ),
-                      Expanded(
-                        child: Container(
-                          height: 180,
-                          padding: EdgeInsets.all(20),
-                          margin: EdgeInsets.only(left: 8),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(14),
-                            color: Colors.black12,
-                          ),
-                          child: Column(
-                            children: [
-                              Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                children: [
-                                  Icon(Icons.waves, color: Color(0xffc3c8cb), size: 35,),
-                                ],
-                              ),
-                              SizedBox(height: 10,),
-                              Text("$windspeed", style: TextStyle(fontFamily: 'Jost', fontWeight: FontWeight.bold, color: Color(0xffc3c8cb), fontSize: 40),),
-                              Text("km/hr", style: TextStyle(fontFamily: 'Jost', fontWeight: FontWeight.normal, color: Color(0xffc3c8cb), fontSize: 16),),
-                            ],
-                          ),
+                      ],
+                    ),
+                    Column(
+                      children: [
+                        Text(
+                          "Humidity",
+                          style: TextStyle(
+                              fontWeight: FontWeight.normal,
+                              fontFamily: 'Jost',
+                              color: Color(0xffc3c8cb),
+                              fontSize: 16),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 40,),
-                  Text("powered by OpenWeatherAPI", style: TextStyle(fontSize: 12, fontStyle: FontStyle.italic, color: Color(
-                      0xffb1b5b7)),),
-
-                ],
-              ),
+                        SizedBox(height: 10),
+                        Text(
+                          "$humidity%",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontFamily: 'Jost',
+                              color: Color(0xffc3c8cb),
+                              fontSize: 18),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ],
             ),
           ),
         ),
       ),
-      backgroundColor: Color(0xff313945),
-
     );
   }
 }
--- weather_app/lib/screens/loading.dart
+++ weather_app/lib/screens/loading.dart
@@ -12,37 +12,34 @@ class LoadingPage extends StatefulWidget {
 }
 
 class _LoadingPageState extends State<LoadingPage> {
-
   var city_name = "loading";
 
-  void callAPI(String city_name) async{
-
+  void callAPI(String city_name) async {
     Weather weather_data = Weather(city: city_name);
     await weather_data.getAPIdata();
 
-    print("Name : "+weather_data.name);
+    print("Name : " + weather_data.name);
 
     setState(() {
-      city_name=weather_data.name;
+      city_name = weather_data.name;
     });
 
-    print("Description : "+weather_data.desc);
-    print("Windspeed : "+weather_data.windspeed);
-    print("Temperature : "+weather_data.temp);
-    print("Humidity : "+weather_data.humidity);
-    print("Icon : "+weather_data.icon);
+    print("Description : " + weather_data.desc);
+    print("Windspeed : " + weather_data.windspeed);
+    print("Temperature : " + weather_data.temp);
+    print("Humidity : " + weather_data.humidity);
+    print("Icon : " + weather_data.icon);
 
-    Future.delayed(Duration(seconds: 1), (){
+    Future.delayed(Duration(seconds: 1), () {
       Navigator.pushReplacementNamed(context, '/home', arguments: {
-        "keyName" : weather_data.name,
-        "keyDesc" : weather_data.desc,
-        "keyWindspeed" : weather_data.windspeed,
-        "keyTemp" : weather_data.temp,
-        "keyHumidity" : weather_data.humidity,
-        "keyIcon" : weather_data.icon,
+        "keyName": weather_data.name,
+        "keyDesc": weather_data.desc,
+        "keyWindspeed": weather_data.windspeed,
+        "keyTemp": weather_data.temp,
+        "keyHumidity": weather_data.humidity,
+        "keyIcon": weather_data.icon,
       });
     });
-
   }
 
   @override
@@ -50,18 +47,16 @@ class _LoadingPageState extends State<LoadingPage> {
     super.initState();
 
     print("init called");
-
   }
 
   @override
   Widget build(BuildContext context) {
     final homeMap = ModalRoute.of(context)!.settings.arguments as Map?;
 
-    if (homeMap?.isNotEmpty ?? false){
+    if (homeMap?.isNotEmpty ?? false) {
       city_name = homeMap?['searchData'];
-    }
-    else{
-      city_name = "Noida";
+    } else {
+      city_name = "Vapi";
     }
     callAPI(city_name);
 
@@ -69,21 +64,55 @@ class _LoadingPageState extends State<LoadingPage> {
       body: Stack(
         children: [
           Expanded(
-              child: Lottie.network("https://assets5.lottiefiles.com/packages/lf20_HlhzUG.json", height: double.infinity, width: double.infinity, fit: BoxFit.cover),),
+            child: Lottie.network(
+                "https://assets5.lottiefiles.com/packages/lf20_HlhzUG.json",
+                height: double.infinity,
+                width: double.infinity,
+                fit: BoxFit.cover),
+          ),
           SingleChildScrollView(
             child: Center(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SizedBox(height: 60,),
-                  Text("$city_name", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Color(0xffe7ebf4), fontFamily: 'Jost'),),
-                  SizedBox(height: 260,),
-                  SpinKitFoldingCube(size: 50, color: Colors.white38,),
-                  SizedBox(height: 240,),
-                  Text("Weather App", style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold, color: Color(0xffe7ebf4), fontFamily: 'Comforter'),),
-                  Text("Discover the world through weather's ever-changing canvas.", style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500, color: Color(0xffe7ebf4), fontStyle: FontStyle.italic, fontFamily: 'Jost'), ),
-
-
+                  SizedBox(
+                    height: 60,
+                  ),
+                  Text(
+                    "$city_name",
+                    style: TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xffe7ebf4),
+                        fontFamily: 'Jost'),
+                  ),
+                  SizedBox(
+                    height: 260,
+                  ),
+                  SpinKitFoldingCube(
+                    size: 50,
+                    color: Colors.white38,
+                  ),
+                  SizedBox(
+                    height: 240,
+                  ),
+                  Text(
+                    "Weather App",
+                    style: TextStyle(
+                        fontSize: 60,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xffe7ebf4),
+                        fontFamily: 'Comforter'),
+                  ),
+                  Text(
+                    "Discover the world through weather's ever-changing canvas.",
+                    style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500,
+                        color: Color(0xffe7ebf4),
+                        fontStyle: FontStyle.italic,
+                        fontFamily: 'Jost'),
+                  ),
                 ],
               ),
             ),
